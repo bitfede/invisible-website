fcdcccd2a59e954a76d2133c7d6a62315ac0e68a
Fede Defaveri <f.g.defaveri@gmail.com>
2018-03-19 16:59:43 +0100

    Installs grid system and tests it succesfully on index

--- src/pages/index.it.js
+++ src/pages/index.it.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import {Container, Row, Col} from 'react-grid-system'
+
 const IndexPage = () => (
   <div>
-    prima sezione
+    <Container>
+      <Row debug>
+        <Col debug sm={12} md={4}>
+        xxx
+        </Col>
+        <Col debug sm={12} md={4}>
+        xxx
+        </Col>
+        <Col debug sm={12} md={4}>
+        xxx
+        </Col>
+      </Row>
+    </Container>
   </div>
 )
 
