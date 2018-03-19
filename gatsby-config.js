e4e303d38beaf1a94563b04412482b52e52b0632
Fede Defaveri <f.g.defaveri@gmail.com>
2018-03-19 16:37:28 +0100

    Adds multilingual support for pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Invisible Glass',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-i18n',
+  ],
 };
