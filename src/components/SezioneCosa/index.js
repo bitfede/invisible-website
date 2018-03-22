77116f18e0fc3c5085b95858b737cbac801d626d
bitfede <f.g.defaveri@gmail.com>
2018-03-22 01:04:28 +0100

    Styles and adds placeholder pictures to cosa section

--- src/components/SezioneCosa/index.js
+++ src/components/SezioneCosa/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
+import {Container, Row, Col, Visible, Hidden} from 'react-grid-system'
+
 import styles from './sezionecosa.module.css'
+
+import leftImg from './sezionecosa-img-left.jpg'
+import rightImg from './remote-img-right.png'
+
 
 class SezioneCosa extends React.Component {
   constructor(props) {
@@ -10,7 +16,22 @@ class SezioneCosa extends React.Component {
   render() {
     return (
       <div>
-        <p>sezionecosa</p>
+        <Container>
+          <Row>
+            <Col sm={12} md={6}>
+              <div className={styles.leftcontainer}>
+                <img className={styles.leftimg} src={leftImg} />
+              </div>
+            </Col>
+
+            <Col sm={12} md={6}>
+            <div className={styles.rightcontainer}>
+              <img className={styles.rightimg} src={rightImg} />
+              <p className={styles.rightpara}>Add some text here please, write a few sentences</p>
+            </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
