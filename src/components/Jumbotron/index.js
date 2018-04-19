36549663c7cf09e2df543bb0031cb8a55964cd32
bitfede <f.g.defaveri@gmail.com>
2018-04-19 14:11:59 +0200

    Changes image on the jumbotron component

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import styles from './jumbotron.module.css'
 
-import pic from './stock-img-1.jpeg'
+import pic from './copertina.jpg'
 
 class Jumbotron extends React.Component{
   constructor (props) {
