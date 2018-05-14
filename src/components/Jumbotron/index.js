e843b0ae9723fa8d07388979e69309b6b9ef8299
bitfede <f.g.defaveri@gmail.com>
2018-05-14 14:29:42 +0200

    Adds the blinking and typing animation to the jumbotron h1 element

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Typewriting from 'react-typewriting'
+
 import styles from './jumbotron.module.css'
 
 import pic from './copertina.jpg'
@@ -13,8 +15,20 @@ class Jumbotron extends React.Component{
       return (
         <div className={styles.maincontainer}>
           <img className={styles.jumboimage} src={pic} />
-          <h1 className={styles.headlinemain}
-          >INVISIBLE GLASS</h1>
+          <h1 className={styles.headlinemain}>La Privacy
+            <Typewriting
+              className={styles.blinkershit}
+              waitBeforeDeleteMs={3000}
+              writeSpeedMs={200}
+              deleteSpeedMs={100}
+              strings={[
+                ' in ufficio',
+                ' in casa',
+                ' dove vuoi',
+                ' con un click!'
+              ]}
+            />
+          </h1>
         </div>
       )
     }
