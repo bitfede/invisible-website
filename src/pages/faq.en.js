6d26ae1bfb1e12dc96ba61ff22dc6bc91ff712ec
bitfede <f.g.defaveri@gmail.com>
2018-05-25 02:15:42 +0200

    Translates the FAQ to english

--- src/pages/faq.en.js
+++ src/pages/faq.en.js
@@ -15,35 +15,36 @@ const FaqPage = () => (
   <SelectLangMini currentPage={"EN"} />
 
   <FaqComponent
-  question1={"E’ possibile modificare il tempo di transizione tra opaco e trasparente?"}
-  answer1={"No, il cambio di stato di INVISIBLE è istantaneo."}
-  question2={"E’ possibile regolare il grado di trasparenza?"}
-  answer2={"Si, mediante l’uso di specifici alimentatori, è possibile regolare il grado di trasparenza, comandandoli attraverso i più comuni dimmer."}
-  question3={"Quando INVISIBLE consuma energia?"}
-  answer3={"INVISIBLE consuma energia solo quando è trasparente, con un consumo indicativo di 7W al metro quadro."}
-  question4={"Che tensione richiede?"}
-  answer4={"INVISIBLE richiede una tensione di 70 V AC a 50Hz/60Hz"}
-  question5={"Qual è lo spessore disponibile di INVISIBLE Glass?"}
-  answer5={"INVISIBLE Glass, è composto da due lastre in vetro e una lamina LCD nel mezzo. Lo spessore delle lastre può essere richiesto a 3,4 o 5 mm.\n | Esempio:  4mm (vetro1) + 1mm (lamina LCD) + 4mm (vetro2) = 9 mm di spessore INVISIBLE Glass"}
-  question6={"Che tipo di cavo è necessario per collegare il trasformatore al vetro?"}
-  answer6={"Due tipi di cavi sono utilizzati per la convenienza dei clienti per quanto riguarda IPX4 o IPX7 norme di sicurezza elettrica."}
-  question7={"Qual è la distanza massima tra il vetro e il trasformatore?"}
-  answer7={"E’ consigliato posizionare il trasformatore il più vicino possibile ai vetri, evitando possibili dispersioni e perdite di potenza, ma comunque ad una distanza massima di 50 m."}
-  question8={"Invisible Glass può avere tacche o buchi?"}
-  answer8={"Sì, INVISIBLE può avere tacche o buchi, dando la possibilità di creare forme e geometrie di ogni genere."}  question9={"A quale temperatura lavora un pannello INVISIBLE?"}
-  answer9={"La temperatura ottimale è compresa tra -20 ° C e + 60 ° C."}
-  question10={"È sicuro toccare INVISIBLE quando è alimentato?"}
-  answer10={"Assolutamente sì. Dal momento che la lamina LCD è stratificata tra due vetri, non rappresenta un rischio. "}
-  question11={"Quali sono le dimensioni massime di INVISIBLE?"}
-  answer11={"Le dimensioni massime di INVISIBLE sono 1.800 mm x 3.750 mm in un unico blocco; per dimensioni maggiori contattateci."}
-  question12={"INVISIBLE può essere stratificato in vetri tripli?"}
-  answer12={"Sì."}
-  question13={"INVISIBLE può essere utilizzato come superfice di proiezione?"}
-  answer13={"Sì. INVISIBLE è proiettabile con un normale videoproiettore. L’effetto migliore si ottiene posizionando il videoproiettore dietro la superfice di INVISIBLE."}
-  question14={"INVISIBLE può essere stratificato con vetri temperati?"}
-  answer14={"Sì. INVISIBLE può essere costituito da due lastre di vetro temprato e temperato extra-chiaro."}
-  question15={"INVISIBLE è di semplice applicazione a pareti già esistenti?"}
-  answer15={"La lamina LCD di INVISIBLE può essere applicata anche a pareti in vetro già esistenti, non richiede l’uso di sapone come le comuni pellicole, quindi garantisce una semplice e rapida installazione."}
+  question1={"Is it possible to modify the transition time from opaque to transparent"}
+  answer1={"No, INVISIBLE's change of state is instant"}
+  question2={"Can we regulate how transparent the glass is?"}
+  answer2={"Yes, by using special power supplies the transparency can be changed, a regular dimmer will do the job."}
+  question3={"When does INVISIBLE consume energy?"}
+  answer3={"INVISIBLE consumes energy only when it is in the transparent state, consuming roughly 7W for each square meter."}
+  question4={"How much tension does it need?"}
+  answer4={"INVISIBLE needs an electrical tension of 70 V AC at 50Hz/60Hz"}
+  question5={"What are the available thicknesses for INVISIBLE Glass?"}
+  answer5={"INVISIBLE Glass, is composed of two sheets of glass plus an LCD sheet. You can request different glass thicknesses such as 3,4 or 5 mm.\n | Example:  4mm (glass1) + 1mm (LCD sheet) + 4mm (glassw) = 9 mm of thickness for this specific INVISIBLE Glass"}
+  question6={"What kind of cable do I need to attach the power supply to the glass?"}
+  answer6={"Clients can choose between IPX4 or IPX7 cables, both compliant to electrical safety norms"}
+  question7={"What's the maximum distance between the power supply and the glass?"}
+  answer7={"We suggest to position the power supply as close as possible to the glasses, so that loss of power is reduced to the minimum. The absolute maximum distance is 50m."}
+  question8={"Can Invisible Glass have holes or dents?"}
+  answer8={"Yes, INVISIBLE can have holes or dents, it is possible to create any shape and geometric figure."}
+  question9={"What is the ideal temperature for INVISIBLE?"}
+  answer9={"The best temperature is between -20 ° C and + 60 ° C."}
+  question10={"Is it safe to touch INVISIBLE when it's on?"}
+  answer10={"Yes of course! Since the LCD sheet is isolated between two other glass sheets there is no safety hazard in touching it"}
+  question11={"What are the maximum dimensions for INVISIBLE?"}
+  answer11={"INVISIBLE's maximum dimensions are 1.800 mm x 3.750 mm in a unique block; contact us to request bigger dimensions."}
+  question12={"INVISIBLE can be done with triple glass stratification instead of double?"}
+  answer12={"Yes."}
+  question13={"Can INVISIBLE be used as a projector-friendly surface?"}
+  answer13={"Yes. INVISIBLE is a projector-friendly surface with any regular videoprojector. For best result position the videoprojector behind INVISIBLE's surface."}
+  question14={"Can INVISIBLE can be installed with tempered glass?"}
+  answer14={"Yes. INVISIBLE can be made with two layers of tempered glass and extra-clear tempered glass."}
+  question15={"Can INVISIBLE be installed in existing glass walls/sheets?"}
+  answer15={"INVISIBLE's LCD layer can be applied to any existing glass surfaces. It does not require the use of soap like the other films, so the installation is fast and easy."}
   question16={""}
   answer16={""}
   question17={""}
